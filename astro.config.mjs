75a4b07324b95ebd307a33f24e362f7dc05bce7a
Ahmed Abbas <ahmed.abbas@nos.nl>
2024-08-16 13:17:57 +0200

    feat: add cloudflare and astro/lit dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from "@astrojs/cloudflare";
+
+import lit from "@astrojs/lit";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: cloudflare(),
+  integrations: [lit()]
+});
